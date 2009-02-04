96acebdce8311eb3c58691f2347fb2eaa13ae680
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-02-04 19:13:38 +0000

    make the template customizable via portal_view_customizations

--- Products/Carousel/browser/viewlet.py
+++ Products/Carousel/browser/viewlet.py
@@ -3,4 +3,12 @@ from plone.app.layout.viewlets.common import ViewletBase
 
 class CarouselViewlet(ViewletBase):
     __name__ = 'Products.Carousel.viewlet'
-    index = ZopeTwoPageTemplateFile('viewlet.pt')
+    
+    def index(self):
+        """
+        Look up Carousel view separately from the viewlet, so the template
+        can be customized via portal_view_customizations (which only knows
+        about global templates), but we can still use a local registration
+        for the viewlet.
+        """
+        return self.context.restrictedTraverse('@@carousel')()
