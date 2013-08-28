563a688ae82c97579738b10b48b938c98db0299c
Dan Jacka <danjacka@gmail.com>
2013-08-28 21:34:21 +1200

    Set exclude_from_nav, for compatibility with plone.app.contenttypes Folders.

--- Products/Carousel/browser/manager.py
+++ Products/Carousel/browser/manager.py
@@ -7,23 +7,32 @@ from Products.Carousel.utils import addPermissionsForRole
 from Products.Carousel.config import CAROUSEL_ID
 
 class CarouselManager(BrowserView):
-    
+
     def __call__(self):
-        
+
         if hasattr(self.context.aq_base, CAROUSEL_ID):
             carousel = getattr(self.context, CAROUSEL_ID)
         else:
             pt = getToolByName(self.context, 'portal_types')
-            newid = pt.constructContent('Folder', self.context, 'carousel', title='Carousel Banners', excludeFromNav=True)
+            newid = pt.constructContent(
+                            type_name='Folder',
+                            container=self.context,
+                            id='carousel',
+                            title='Carousel Banners',
+                            excludeFromNav=True, # for Archetypes Folders
+                            exclude_from_nav=True, # for Dexterity Folders
+                        )
             carousel = getattr(self.context, newid)
-            
+
             # mark the new folder as a Carousel folder
             alsoProvides(carousel, ICarouselFolder)
-            
+
             # make sure Carousel banners are addable within the new folder
-            addPermissionsForRole(carousel, 'Manager', ('Carousel: Add Carousel Banner',))
-            addPermissionsForRole(carousel, 'Site Administrator', ('Carousel: Add Carousel Banner',))
-            
+            addPermissionsForRole(carousel, 'Manager',
+                                  ('Carousel: Add Carousel Banner',))
+            addPermissionsForRole(carousel, 'Site Administrator',
+                                  ('Carousel: Add Carousel Banner',))
+
             # make sure *only* Carousel banners are addable
             aspect = ISelectableConstrainTypes(carousel)
             aspect.setConstrainTypesMode(1)
